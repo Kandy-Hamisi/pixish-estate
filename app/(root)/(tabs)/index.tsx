0e15de9f12eabdee87bd7a9288857c6a1600589a
Kandy-Hamisi <hkandy2000@gmail.com>
2025-04-12 23:32:09 +0300

    added featre card using a flatlist

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -9,7 +9,7 @@ import {
 import { Link, useRouter } from "expo-router";
 import { logout } from "@/lib/appwrite";
 import { SafeAreaView } from "react-native-safe-area-context";
-import React from "react";
+import React, { useState } from "react";
 import images from "@/constants/images";
 import { useGlobalContext } from "@/lib/global-provider";
 import Search from "@/components/Search";
@@ -17,21 +17,25 @@ import { Card, FeatureCard } from "@/components/Cards";
 import Filters from "@/components/Filters";
 
 export default function Index() {
+  const [numColumns, setNumColumns] = useState(2);
   const router = useRouter();
   const { user } = useGlobalContext();
   return (
     <SafeAreaView className="bg-white h-full">
       <FlatList
-        data={[1, 2, 3, 4]}
+        data={[1, 2, 3, 4, 5, 6]}
         renderItem={({ item }) => <Card />}
         keyExtractor={(item) => item.toString()}
-        numColumns={2}
+        numColumns={numColumns}
+        contentContainerClassName="pb-32"
+        columnWrapperClassName="flex gap-5 px-5"
+        showsVerticalScrollIndicator={false}
         ListHeaderComponent={
           <View className="px-5">
             <View className="flex flex-row items-center justify-between mt-5">
               <View className="flex flex-row items-center">
                 <Image
-                  source={images.avatar}
+                  source={{ uri: user?.avatar }}
                   className="size-12 rounded-full"
                 />
                 <View className="flex flex-col items-start ml-2 justify-center">
@@ -57,6 +61,15 @@ export default function Index() {
                   </Text>
                 </TouchableOpacity>
               </View>
+              <FlatList
+                data={[1, 2, 3, 4]}
+                horizontal
+                contentContainerClassName="flex gap-5 mt-5"
+                keyExtractor={(item) => item.toString()}
+                bounces={false}
+                showsHorizontalScrollIndicator={false}
+                renderItem={({ item }) => <FeatureCard />}
+              />
               {/*<View className="flex flex-row gap-5 mt-5">*/}
               {/*  <FeatureCard />*/}
               {/*  <FeatureCard />*/}
